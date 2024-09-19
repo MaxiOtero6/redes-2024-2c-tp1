68ed2b5be7a9fd53f2f8e7bea70ac47aa6989028
JM-Pascual <jmpascualosorio@gmail.com>
2024-09-19 18:27:24 -0300

    Feat: First implementation of the file sending logic. First implementation of the file sending logic between uploader and server.

--- src/lib/server.py
+++ src/lib/server.py
@@ -10,15 +10,39 @@ class Server:
         self.__skt : socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__skt.bind((config.HOST, config.PORT))
 
-    def run(self):
+    def __recv_file_name(self):
         (data, address) = self.__skt.recvfrom(520)
         packet = SWPacket.decode(data)
-        print(f"Packet sequence N°: {packet.seq_number}")
-        print(f"Packet ACK N°: {packet.ack_number}")
-        print(f"Packet SYN: {packet.syn}")
-        print(f"Packet FIN: {packet.fin}")
-        print(f"Packet payload: {packet.payload}")
-        response_data = SWPacket(1, 1, False, True, True, b"ACK").encode()
-        self.__skt.sendto(response_data, address)
+        print(f"Received packet from {address}")
+        file_name = packet.payload.decode()
+        print(f"File name received: {file_name}")
+        response = SWPacket(packet.ack_number, packet.seq_number, True, False, True, b"")
+        self.__skt.sendto(response.encode(), address)
+        return file_name
+
+    def __ack_file_received(self, address : (str, int)):
+        response = SWPacket(0, 1, True, False, True, b"")
+        self.__skt.sendto(response.encode(), address)
+
+    def __recv_file_data(self, file_path: str):
+        (data, address) = self.__skt.recvfrom(520)
+        packet = SWPacket.decode(data)
+        last_seq_number = packet.seq_number
+        while not packet.fin:
+            if last_seq_number != packet.seq_number:
+                with open(file_path, "wb") as file:
+                    file.write(packet.payload)
+                    last_seq_number = packet.seq_number
+            response = SWPacket(packet.ack_number, packet.seq_number, False, False, True, b"")
+            self.__skt.sendto(response.encode(), address)
+            (data, address) = self.__skt.recvfrom(520)
+            packet = SWPacket.decode(data)
+        self.__ack_file_received(address)
+
+
+    def run(self):
+        file_name = self.__recv_file_name()
+        file_path = f"{self.__storage_dir}/{file_name}"
+        self.__recv_file_data(file_path)
+        print(f"File received: {file_name}")
         self.__skt.close()
-        print("Connection closed")
--- src/lib/upload_client.py
+++ src/lib/upload_client.py
@@ -10,15 +10,60 @@ class UploadClient:
         self.__source_path : str = config.SOURCE_PATH
         self.__file_name : str = config.FILE_NAME
 
+        self.__sequence_number = 0
+
+    def __swap_sequence_number(self):
+        self.__sequence_number = 1 if self.__sequence_number == 0 else 0
+
+    def __send_file_name(self):
+        file_name_package = SWPacket(self.__sequence_number,
+                                     1 if self.__sequence_number == 0 else 0,
+                                     True, False, False, self.__file_name.encode())
+        self.__skt.sendto(file_name_package.encode(), self.__destination_address)
+        self.__swap_sequence_number()
+        print(f"File name sent: {self.__file_name}")
+        (response, address) = self.__skt.recvfrom(520)
+        response_packet = SWPacket.decode(response)
+        while not response_packet.ack or response_packet.ack_number == self.__sequence_number:
+            self.__skt.sendto(file_name_package.encode(), self.__destination_address)
+            response = self.__skt.recv(520)
+            response_packet = SWPacket.decode(response)
+        print(f"File name ack received")
+
+    def __send_file_data(self):
+        with open(self.__source_path, "rb") as file:
+            data = file.read(512)
+            while len(data) != 0:
+                packet = SWPacket(self.__sequence_number,
+                                  1 if self.__sequence_number == 0 else 0,
+                                  False, False, False, data)
+                self.__skt.sendto(packet.encode(), self.__destination_address)
+                response = self.__skt.recv(520)
+                response_packet = SWPacket.decode(response)
+
+                if response_packet.ack and self.__sequence_number == response_packet.ack_number:
+                    print(f"Packet of size {len(data)} sent")
+                    data = file.read(512)
+                    self.__swap_sequence_number()
+                else:
+                    continue
+
+    def __send_comm_fin(self):
+        fin_packet = SWPacket(self.__sequence_number,
+                              1 if self.__sequence_number == 0 else 0,
+                              False, True, False, b"")
+        self.__skt.sendto(fin_packet.encode(), self.__destination_address)
+        response = self.__skt.recv(520)
+        response_packet = SWPacket.decode(response)
+        while not response_packet.ack or response_packet.ack_number != self.__sequence_number:
+            self.__skt.sendto(fin_packet.encode(), self.__destination_address)
+            response = self.__skt.recv(520)
+            response_packet = SWPacket.decode(response)
+
     def run(self):
-        data_to_send = SWPacket(1, 1, False, True, False, b"Hello, world!").encode()
-        self.__skt.sendto(data_to_send, self.__destination_address)
-        (data, address) = self.__skt.recvfrom(520)
-        packet = SWPacket.decode(data)
-        print(f"Packet sequence N°: {packet.seq_number}")
-        print(f"Packet ACK N°: {packet.ack_number}")
-        print(f"Packet SYN: {packet.syn}")
-        print(f"Packet FIN: {packet.fin}")
-        print(f"Packet payload: {packet.payload}")
+        print("Starting file upload")
+        self.__send_file_name()
+        self.__send_file_data()
+        self.__send_comm_fin()
         self.__skt.close()
-        print("Connection closed")
+        print("File sent")
